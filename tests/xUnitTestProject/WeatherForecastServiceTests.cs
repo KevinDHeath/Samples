da05ffac5f9fa1f9bb9597a8bab47fe30be2712d
Kevin D. Heath <87247475+KevinDHeath@users.noreply.github.com>
2024-03-23 18:29:45 -0400

    Add unit tests to increase code coverage (#6)

---
+++ tests/xUnitTestProject/WeatherForecastServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibrary.Services;
+
+namespace xUnitTestProject;
+
+public class WeatherForecastServiceTests
+{
+	[Fact]
+	public void WeatherForecastService_Get_Should_Return_5()
+	{
+		// Arrange
+		WeatherForecastService service = new();
+
+		// Act (with code coverage)
+		var result = service.Get().ToList();
+		if( result.Count > 0 ) { _ = result[0].TemperatureF; }
+
+		// Assert
+		result.Should().HaveCount( 5 );
+	}
+
+	[Fact]
+	public void WeatherForecastServiceEx_Get_Should_Return_15()
+	{
+		// Arrange
+		WeatherForecastServiceEx service = new();
+
+		// Act
+		var result = service.Get();
+
+		// Assert
+		result.Should().HaveCount( 15 );
+	}
+}
